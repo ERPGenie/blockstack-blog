91906ba0ee12781fc23d41b274271112ca2cfd32
erpgenie <30003201+ERPGenie@users.noreply.github.com>
2020-03-18 16:06:41 -0700

    Update name

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -10,7 +10,7 @@ export default class Signin extends Component {
 
     return (
       <div className="panel-landing" id="section-1">
-        <h1 className="landing-heading">Publik</h1>
+        <h1 className="landing-heading">Blockstack Blogging</h1>
         <p>Decentralized micro-blogging</p>
         <p className="lead">
           <button
